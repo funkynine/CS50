4c94fcd347f22700471c49be75423a65f2920e12
Igor Bazyliuk <igorbazyliuk@hack.local>
2019-01-22 20:42:35 +0200

    add func work with api

--- api.py
+++ api.py
@@ -12,6 +12,22 @@ data = res.json()
 temp = data['main']['temp']
 cityName = data['name']
 country = data['sys']['country']
+lat, lon = data['coord']['lat'], data['coord']['lon']
 
+
+pprint(data)
 
 print("Temp: {0}, Name: {1}, {2}".format(temp, cityName, country))
+
+def dataAboutCity(city):
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=a844a051424724097641db263e8f5bc9&units=metric'.format(city)
+
+    res = request.get(url)
+    data = res.json()
+
+    temp = data['main']['temp']
+    cityName = data['name']
+    country = data['sys']['country']
+    description = data['weather']['description']
+    lat, lon = data['coord']['lat'], data['coord']['lon']
