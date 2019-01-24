8dcfcefc7e0fe1b72b20790d859dab506a6dee84
Igor Bazyliuk <igorbazyliuk@hack.local>
2019-01-24 13:37:17 +0200

    the end for cs50

--- mysite/static/myvenv/script.js
+++ mysite/static/myvenv/script.js
@@ -1,6 +1,19 @@
+let map;
 
+function initMap() {
+mapOptions = {
+          zoom: 8,
+          center: {lat: 50.401, lng: 30.252}
+        };
+    map =new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+}
 
-function infoAboutCity(event) {
+function changeCenter(latitude, longitude){
+  map.setCenter({lat: latitude, lng: longitude});
+}
+
+function infoAboutCity() {
     cityName = document.getElementById('cityName').value
     data = {
         city: cityName,
@@ -19,7 +32,7 @@ function addInfo(payload, url){
   })
   .then((res) => res.json())
   .then((data) => {
-    takeInfo(data)
+      takeInfo(data)
   })
   .catch((err) => {
       console.warn('this is error',err)
@@ -27,5 +40,29 @@ function addInfo(payload, url){
 }
 
 function takeInfo(data){
-    document.getElementById('cityName').value = data.data
+
+
+    console.log(data)
+
+    switch (data.cod) {
+        case '404':
+            alert (data.message)
+            break;
+        default:
+    }
+
+    var temp = data.main.temp
+    var cityName = data.name
+    var country = data.sys.country
+    var main = data.weather[0].description
+    var lat = data.coord.lat
+    var lon = data.coord.lon
+
+    console.log(lat, lon)
+
+    document.getElementById('nameCity').innerHTML = cityName + ' ' + country
+    document.getElementById('tempCity').innerHTML = 'Temp: ' + temp
+    document.getElementById('description').innerHTML = main
+
+    changeCenter(lat, lon)
 }
--- mysite/views.py
+++ mysite/views.py
@@ -5,6 +5,7 @@ import requests
 import sys
 import json
 
+
 # Create your views here.
 
 # def post_list(request):
@@ -20,6 +21,7 @@ def test(request):
 @csrf_exempt
 def api(request):
     if request.method == 'POST':
+
         user = json.loads(request.body)
         city = user['city']
         url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=a844a051424724097641db263e8f5bc9&units=metric'.format(city)
@@ -28,28 +30,9 @@ def api(request):
         data = res.json()
 
         if data['cod'] == 200:
-            temp = data['main']['temp']
-            cityName = data['name']
-            country = data['sys']['country']
-            lat, lon = data['coord']['lat'], data['coord']['lon']
-            return JsonResponse({'data': temp}, status=200)
+            return JsonResponse(data, status=200)
         else:
-            print(data['message'])
-            return JsonResponse({'data':data['message']}, status=208)
+            return JsonResponse(data, status=208)
     else:
         print('GET')
         return HttpResponse('GET')
-#
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=a844a051424724097641db263e8f5bc9&units=metric'.format(city)
-
-    res = requests.get(url)
-    data = res.json()
-
-    if data['cod'] == 200:
-        temp = data['main']['temp']
-        cityName = data['name']
-        country = data['sys']['country']
-        lat, lon = data['coord']['lat'], data['coord']['lon']
-        print("Temp: {0}, Name: {1}, {2}".format(temp, cityName, country))
-    else:
-        print(data['message'])
